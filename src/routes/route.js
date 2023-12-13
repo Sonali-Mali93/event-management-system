90b9534d0a5bb62760ee1580de88475a7e66a6e8
Sonali Mali <sonalimali999@gmail.com>
2023-12-13 14:43:36 +0530

    feat: routes path for REST API

---
+++ src/routes/route.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const aws = require('aws-sdk')
+const UserController = require("../controllers/userController")
+const EventController = require("../controllers/eventController")
+const InvitationController = require("../controllers/invitationController")
+const mid = require("../middlware/middlware")
+const { route } = require('express/lib/application');
+
+
+//1.  Create (User can create Event)
+router.post('/register', UserController.CreateUser)
+
+router.post("/login", UserController.logIn)
+
+// 1.  Create (User can create Event)
+router.post("/event", mid.authentication, EventController.createEvent)
+
+router.get("/event", mid.authentication, EventController.getEvent)
+
+// 2. Invite (Users) // User can see his created event list and also events in which he is invited.
+router.post("/invite/:eventId", mid.authentication, mid.authorization, InvitationController.inviteUsers)
+
+// 4. Event Detail + list of users invited (API to get specific event and invites)
+router.get('/eventDetail/:eventId', mid.authentication, mid.authorization, EventController.eventDetail);
+
+//  5. Event update (Event Update)
+router.put("/updateevent/:eventId", mid.authentication, mid.authorization, EventController.updateEvent)
+
+
+module.exports = router
